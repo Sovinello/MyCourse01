dc5f1077bb3d4c2c65fe018cdfbf2cf73233a24a
a.ranaldi <a.ranaldi@almaviva.it>
2020-02-07 10:15:12 +0100

    Primo commit

---
+++ MyCourse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace MyCourse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string arg01 = args.FirstOrDefault();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //KESTREL => webserver di default di .netCore
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
